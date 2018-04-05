82b94f3f44a83a2ef6d0d36a5e0d6207cd229ac5
Willy Scheibel <willyscheibel@gmx.de>
2018-04-05 17:53:49 +0200

    Update khrbinding generation

--- source/glscbinding/include/glscbinding/Version.h
+++ source/glscbinding/include/glscbinding/Version.h
@@ -22,9 +22,9 @@ namespace glscbinding
 *
 *  Example code:
 *  \code{.cpp}
-*  const glscbinding::Version currentVersion = glscbinding::aux::ContextInfo::version();
+*  const glbinding::Version currentVersion = glbinding::aux::ContextInfo::version();
 *
-*  if (currentVersion >= glscbinding::Version(3, 2))
+*  if (currentVersion >= glbinding::Version(3, 2))
 *  {
 *      // do something
 *  }
